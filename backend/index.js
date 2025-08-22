5339cccf58e6f7ee534c7768da1938593b8e773e
yanswo <yanlucas2202@gmail.com>
2025-08-22 09:00:06 -0300

    initial node

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log("Acessou a rota raiz");
+  res.send("Hello World");
+});
+
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
